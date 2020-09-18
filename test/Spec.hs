cdde84a9d11d694e6c0258bb65f681009a1d3e20
JustRoxy <25293265+JustRoxy@users.noreply.github.com>
2020-09-18 19:54:37 +0300

    Refactoring

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
-import Test.Hspec
-import Test.QuickCheck
-import Data.Char
-import Lib
+import           Data.Char
+import           Lib
+import           Test.Hspec
+import           Test.QuickCheck
 
 main :: IO ()
 main = encoding64Test
@@ -11,28 +11,28 @@ ascii = elements $ map chr [0..255]
 
 encoding64Test :: IO ()
 encoding64Test = hspec $ do
-    describe "correct paddings" $ do
-        it "Base64" $ do
-            padd (getRes $ encode64 "Man") `shouldBe` (0 :: Int)
-            padd (getRes $ encode64 "Mann") `shouldBe` (2 :: Int)
-            padd (getRes $ encode64 "Mannn") `shouldBe` (1 :: Int)
-        it "Base32" $ do
-            padd (getRes $ encode32 "Man") `shouldBe` (3 :: Int)
-            padd (getRes $ encode32 "Mann") `shouldBe` (1 :: Int)
-            padd (getRes $ encode32 "Mannn") `shouldBe` (0 :: Int)
-        it "Base16" $ do
-            padd (getRes $ encode16 "Man") `shouldBe` (0 :: Int)
-            padd (getRes $ encode16 "Man") `shouldBe` (0 :: Int)
-            padd (getRes $ encode16 "Man") `shouldBe` (0 :: Int)
+  describe "correct paddings" $ do
+    it "Base64" $ do
+      padd (getRes $ encode64 "Man") `shouldBe` (0 :: Int)
+      padd (getRes $ encode64 "Mann") `shouldBe` (2 :: Int)
+      padd (getRes $ encode64 "Mannn") `shouldBe` (1 :: Int)
+    it "Base32" $ do
+      padd (getRes $ encode32 "Man") `shouldBe` (3 :: Int)
+      padd (getRes $ encode32 "Mann") `shouldBe` (1 :: Int)
+      padd (getRes $ encode32 "Mannn") `shouldBe` (0 :: Int)
+    it "Base16" $ do
+      padd (getRes $ encode16 "Man") `shouldBe` (0 :: Int)
+      padd (getRes $ encode16 "Man") `shouldBe` (0 :: Int)
+      padd (getRes $ encode16 "Man") `shouldBe` (0 :: Int)
 
-    describe "round test" $ do
-        it "Base64" $ property $
-            quickCheck (sample' ascii >>= (\x -> getRes (decode64 =<< encode64 x) `shouldBe` x))
-        it "Base32" $ property $
-            quickCheck (sample' ascii >>= (\x -> getRes (decode32 =<< encode32 x) `shouldBe` x))
-        it "Base16" $ property $
-            quickCheck (sample' ascii >>= (\x -> getRes (decode16 =<< encode16 x) `shouldBe` x))
-        where 
-            getRes (Left b) = b
-            getRes (Right b) = b
-            padd = length . filter (== '=')
+  describe "round test" $ do
+    it "Base64" $ property $
+      quickCheck (sample' ascii >>= (\x -> getRes (decode64 =<< encode64 x) `shouldBe` x))
+    it "Base32" $ property $
+        quickCheck (sample' ascii >>= (\x -> getRes (decode32 =<< encode32 x) `shouldBe` x))
+    it "Base16" $ property $
+        quickCheck (sample' ascii >>= (\x -> getRes (decode16 =<< encode16 x) `shouldBe` x))
+  where
+    getRes (Left b)  = b
+    getRes (Right b) = b
+    padd = length . filter (== '=')

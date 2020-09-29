911ab8d9090a737df052df9d4e70d56be8245e19
JustRoxy <25293265+JustRoxy@users.noreply.github.com>
2020-09-29 18:19:51 +0300

    Revert "Make vector of VectorContent Storable"

--- src/BasedArray.hs
+++ src/BasedArray.hs
@@ -1,44 +1,32 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 module BasedArray
     ( fromString
     , example
     , mkSlice
-    , vectorPointer
-    , recreateVector
     ) where
 
-import           Data.Char                  (ord)
-import           Data.Word                  (Word8)
-import           Foreign.ForeignPtr
-import           Foreign.Storable
-import qualified Language.Haskell.TH        as TH
-import           Language.Haskell.TH.Syntax
+import           BaseCrypto
+import           Control.Applicative
+import qualified Data.Vector         as V
+import           Data.Vector.Storable.Mutable (Storable)
 import           QuasiQuoter
---               Well, there was a Storable vector after all. Idk how did i missed that
-import           Data.Vector.Storable       as VS
+import           Data.Char (ord)
+import           Data.Word (Word8)
+import qualified Language.Haskell.TH as TH
+import           Language.Haskell.TH.Syntax
 
 newtype VectorContent = VectorContent Word8
-  deriving (Eq, Show, Storable)
+  deriving (Show, Storable)
 
-fromString :: String -> VS.Vector VectorContent
-fromString x = VS.fromList (Prelude.map (VectorContent . fromIntegral . ord) x)
+fromString :: String -> V.Vector VectorContent
+fromString x = V.fromList (map (VectorContent . fromIntegral . ord) x)
 
-example :: VS.Vector VectorContent
+example :: V.Vector VectorContent
 example = fromString [base64|ZXhhbXBsZQ==|]
 
--- | The result of this function looks like (Memory pointer, start, offset):(0x000000000730d2a0,0,7)
-vectorPointer :: (Storable a) => VS.Vector a -> (ForeignPtr a, Int, Int)
-vectorPointer = unsafeToForeignPtr
--- | The function takes ForeignPtr, start and offset, and recreates StorableVector of a, where a should be Storable
--- | let (p, s, o) = vectorPointer x
--- | recreateVector p s o == x
-recreateVector :: (Storable a) => ForeignPtr a -> Int -> Int -> Vector a
-recreateVector = unsafeFromForeignPtr
-
-slice :: (Storable a) => Int -> Int -> VS.Vector a -> VS.Vector a
-slice = VS.slice
+slice = V.slice
 
 mkSlice :: Integer -> Integer -> TH.Name -> TH.ExpQ
 mkSlice x y s= apply
@@ -50,6 +38,6 @@ mkSlice x y s= apply
                     (intVar y)) --applying y to slice x (slice x y)
                   (variable s) --applying s to slice x y (slice x y s)
     where
-      apply = TH.appE
+      apply x y = TH.appE x y
       intVar = TH.litE . IntegerL
       variable = TH.varE
--- src/QuasiQuoter.hs
+++ src/QuasiQuoter.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+
 
 module QuasiQuoter
     ( base64
@@ -7,10 +8,11 @@ module QuasiQuoter
     , base16
     ) where
 
-import           BaseCrypto
-import           Data.Char                 (isAlpha, isDigit)
-import qualified Language.Haskell.TH       as TH
+import qualified Language.Haskell.TH as TH
 import           Language.Haskell.TH.Quote
+import           BaseCrypto
+import           Data.Char (isAlpha, isDigit)
+import           Control.Applicative
 
 baser :: (String -> TH.ExpQ) -> QuasiQuoter
 baser x = QuasiQuoter { quoteExp = x,
@@ -19,16 +21,13 @@ baser x = QuasiQuoter { quoteExp = x,
                         quoteType = undefined
         }
 
-base64 :: QuasiQuoter
 base64 = baser (\s -> [|runBase decode64 s|])
-
-base32 :: QuasiQuoter
 base32 = baser (\s -> [|runBase decode32 s|])
-
-base16 :: QuasiQuoter
 base16 = baser (\s -> [|runBase decode16 s|])
 
 runBase :: (String -> Either Error String) -> String -> String
-runBase f v = case f $ filter (\x -> isAlpha x || isDigit x || x `elem` "=_-") v of
-              Right a -> a
-              Left a  -> "Error: " ++ a
+runBase f x = case f $ filter (\x -> isAlpha x || isDigit x || x `elem` "=_-") x of
+              Right x -> x
+              Left x -> "Error: " ++ x
+
+
